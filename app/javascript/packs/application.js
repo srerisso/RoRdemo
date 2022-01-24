4b9d598b4dece8455a7041001e803d8d8185f8f3
erisso <works@erisso.com>
2022-01-24 13:30:47 +0100

    Rails 6 and Fontawesome integrations.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import '../bootstrap_js_file'
-import './demo/chart-area-demo'
-import './demo/chart-bar-demo'
-import './demo/chart-pie-demo'
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import '../bootstrap_js_file';
+import './demo/chart-area-demo';
+import './demo/chart-bar-demo';
+import './demo/chart-pie-demo';
 // require('bootstrap')
-require('@fortawesome/fontawesome-free/js/all')
-require('./sb-admin-2')
+// import "@fortawesome/fontawesome-free/css/all.css";
+import '../font-awesome'
+import "./sb-admin-2";
 
 Rails.start()
 Turbolinks.start()
